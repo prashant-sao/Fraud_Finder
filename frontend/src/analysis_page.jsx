86879d9281ba3250396acb4ed9b522232d589387
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-11-11 21:34:06 +0530

    .....

--- frontend/src/analysis_page.jsx
+++ frontend/src/analysis_page.jsx
@@ -270,7 +270,7 @@ const AnalysisPage = () => {
                                                                 borderRadius: 12,
                                                                 width: 100,
                                                                 height: 100,
-                                                                minWidth: 250,
+                                                                minWidth: 200,
                                                                 minHeight: 100,
                                                                 maxWidth: 100,
                                                                 maxHeight: 100,
